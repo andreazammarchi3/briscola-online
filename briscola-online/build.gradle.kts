6aa5a714d1256a069c5584fe002588a4323c00a3
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2023-05-02 17:54:25 +0200

    Update build.gradle.kts

--- briscola-online/build.gradle.kts
+++ briscola-online/build.gradle.kts
@@ -20,8 +20,11 @@ subprojects {
     tasks.test {
         useJUnitPlatform()
     }
+    /*
     java {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
+     */
 }
